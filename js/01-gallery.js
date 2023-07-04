92330d7fe32800ef854ff09a160a9a50e7688b3e
OleksiiMinnikov <89975657+OleksiiMinnikov@users.noreply.github.com>
2023-07-04 15:06:12 +0300

    add one

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-
 const galleryContainerEl = document.querySelector(".gallery");
 const imagesMarkup = createItemsMarkup(galleryItems);
 galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
